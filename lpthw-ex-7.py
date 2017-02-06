957a452b8e561649b7b356e578f80d88a5cef089
neal.rogers@spsmail.cuny.edu <neal.rogers@spsmail.cuny.edu>
2017-02-06 11:35:48 -0500

    Completed exercise 7

--- lpthw-ex-7.py
+++ lpthw-ex-7.py
@@ -3,29 +3,26 @@
 """This program simply prints output."""
 
 
-x = "There are %d types of people." % 10
-binary = "binary"
-do_not = "don't"
-y = "Those who know %s and those who %s." % (binary, do_not)
+print "Mary had a little lamb." # This line prints a string.
+print "Its fleece is white as %s." % 'snow' # This line prints a string.
+print "And everywhere that Mary went." # This line prints a string.
+print "." * 10 # what'd they do? # This line prints a '.' 10 times.
 
 
-print x
-print y
+end1 = "C" # This line sets a variable value.
+end2 = "h" # This line sets a variable value.
+end3 = "e" # This line sets a variable value.
+end4 = "e" # This line sets a variable value.
+end5 = "s" # This line sets a variable value.
+end6 = "e" # This line sets a variable value.
+end7 = "B" # This line sets a variable value.
+end8 = "u" # This line sets a variable value.
+end9 = "r" # This line sets a variable value.
+end10 = "g" # This line sets a variable value.
+end11 = "e" # This line sets a variable value.
+end12 = "r" # This line sets a variable value.
 
 
-print "I said: %r." % x
-print "I also said: '%s'." % y
-
-
-hilarious = False
-joke_evaluation = "Isn't that joke so funny?! %r"
-
-
-print joke_evaluation % hilarious
-
-
-w = "This is the left side of..."
-e = "a string with a right side."
-
-
-print w + e
+# watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6, # This line prints variable values.
+print end7 + end8 + end9 + end10 + end11 + end12 # This line prints variable values.
