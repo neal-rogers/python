2ef600977c6d8bfa562a97131d2cd8c4cecc6735
neal.rogers@spsmail.cuny.edu <neal.rogers@spsmail.cuny.edu>
2017-02-10 13:52:36 -0500

    Added exercise 11 and completed exercise 10

--- lpthw-ex-10.py
+++ lpthw-ex-10.py
@@ -3,4 +3,30 @@
 """This program simply prints output."""
 
 
-derp
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+lazy_cat = "\v This is a vertical tab."
+silly_cat = "This line has a carriage return right \r here."
+dumb_cat = "This line has an ASCII bell right \a here."
+
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+print lazy_cat
+print silly_cat
+print dumb_cat
+
+while True:
+    for i in ["/","-","|","\\","|"]:
+        print "%s\r" % i,
