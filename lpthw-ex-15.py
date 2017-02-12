491d38b0bc61cfb3580283310a6422975f6b6848
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-02-12 01:59:11 -0500

    Added exercise 15 sample text file and completed exercise 15

--- lpthw-ex-15.py
+++ lpthw-ex-15.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This program simply prints output."""
+"""This program reads a file and does stuff."""
 
 
+from sys import argv
+
+#script, filename = argv
+
+print "Enter the filename:"
+filename = raw_input("> ")
+
+txt = open(filename)
+
+print "Here's your file %r:" % filename
+print txt.read()
+
+txt.close()
+open(filename)
+
+print "Type the filename again:"
+file_again = raw_input("> ")
+
+txt_again = open(file_again)
+
+print txt_again.read()
+txt_again.close()
