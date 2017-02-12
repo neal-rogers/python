ce5ada665529138be6bacd999de5df33f707ac48
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-02-12 01:03:17 -0500

    Added arguments to exercise 14

--- lpthw-ex-14.py
+++ lpthw-ex-14.py
@@ -5,8 +5,8 @@
 
 from sys import argv
 
-script, user_name = argv
-prompt = ' '
+script, user_name, age = argv
+prompt = '> '
 
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
@@ -19,8 +19,13 @@ lives = raw_input(prompt)
 print "What kind of computer do you have?",
 computer = raw_input(prompt)
 
+print "This last question is about your %s." % age
+print "What is your %s?" % age,
+age = raw_input(prompt)
+
 print """
 Alright, so you said %r about liking me.
 You live in %r. Not sure where that is.
-And you have a %r computer. Nice.
-""" % (likes, lives, computer)
+You have a %r computer. Nice.
+And you are %r years old. Wow.
+""" % (likes, lives, computer, age)
