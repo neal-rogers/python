fbd21fe97f8aae91edaf74b03fa18b101fa3a3fb
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-02-11 23:08:10 -0500

    Completed initial task of exercise 11

--- lpthw-ex-11.py
+++ lpthw-ex-11.py
@@ -3,4 +3,14 @@
 """This program simply prints output."""
 
 
-derp
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+
+print "So, you're %r old, %r tall, and %r heavy." % (
+    age, height, weight
+)
